31eb928706d0460b10ad8f52fc5b9f94d6061c12
Jash Patel <jashpp@gmail.com>
2012-11-21 10:48:43 -0500

    JS to submit form.

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html', name=None)
 
+@app.route('/add', methods=['POST'])
+def add():
+    return "OK"
+
 
 if __name__ == '__main__':
     app.run()
