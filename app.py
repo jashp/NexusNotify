01e23500a179031ae9b0e71c082a8b190e6b2ed4
Jash Patel <jashpp@gmail.com>
2012-11-24 16:10:36 -0500

    Implemented adding emails.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import *
+import MySQLdb
+from local_settings import *
+
 app = Flask(__name__)
+db = MySQLdb.connect(host=DB_HOST,user=DB_USER,passwd=DB_PASS,db=DB_NAME)
 
 @app.route('/')
 def hello_world():
@@ -7,7 +11,14 @@ def hello_world():
 
 @app.route('/add', methods=['POST'])
 def add():
-    return "OK"
+	versions = request.form.getlist("inputVersion[]")
+	email = request.form["inputEmail"]
+	location = request.form["inputLocation"]
+	num_versions = len(versions)
+	c = db.cursor()
+	c.executemany("INSERT INTO emails (email, location, version) VALUES (%s, %s,%s) ON DUPLICATE KEY UPDATE sent=0, unsubscribe=0", zip([email]*num_versions, [location]*num_versions, versions))
+	db.commit()
+	return "OK"
 
 
 if __name__ == '__main__':
