84a03d77bd586adf3141755330bc72b00cef65d0
Jash Patel <jashpp@gmail.com>
2012-12-10 20:56:47 -0500

    Added UK and Australia to list.

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ db = MySQLdb.connect(host=DB_HOST,user=DB_USER,passwd=DB_PASS,db=DB_NAME)
 
 class AddForm(Form):
 	email = TextField('email', validators=[Required(), Email()])
-	location = SelectField('location', choices=[('0', 'United States'), ('1', 'Canada')], validators=[Required()])
+	location = SelectField('location', choices=[('0', 'United States'), ('1', 'Canada'), ('2', 'United Kingdom'), ('3', 'Australia')], validators=[Required()])
 	versions = SelectMultipleField('versions', choices=[('0', 'Nexus 4 (8gb)'), ('1', 'Nexus 4 (16gb)'), ('2', 'Nexus 4 Bumper')], option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False) , validators=[Required()])
 	if not app.debug:
 		recaptcha = RecaptchaField()
